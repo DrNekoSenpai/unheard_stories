772c9729a9b0a5d3f767064654aa9819f32f00fa
DrNekoSenpai <haesungbecker@gmail.com>
2023-05-30 16:25:39 -0700

    Revert "fixed the user not staying signed in when viewing or submitting, moved the html file to the beginning of @action.uses params"
    This reverts commit 9569bc17ae97abd17c39f0f50a58a2e8849998f7.

--- controllers.py
+++ controllers.py
@@ -4,8 +4,7 @@ from py4web                  import action, request, abort, redirect, URL, Field
 from py4web.utils.form       import Form, FormStyleBulma
 from py4web.utils.url_signer import URLSigner
 
-from yatl.helpers            import A
-from . common                import db, session, T, cache, auth, signed_url
+from . common import db, session, T, cache, auth, signed_url
 
 url_signer = URLSigner(session)
 
@@ -53,9 +52,65 @@ def add_story():
     )
     return "ok"
 
+@action('submit', method=["GET", "POST"])
+@action.uses(db, session, auth.user, 'submit.html')
+def submit():
+    form = Form(db.story, csrf_session=session, formstyle=FormStyleBulma)
+    if form.accepted:
+        # We simply redirect; the insertion already happened.
+        redirect(URL('index'))
+    # Either this is a GET request, or this is a POST but not accepted = with errors.
+    return dict(form=form)
 
-@action('view', method="POST")
-@action.uses('view.html', db, url_signer, auth.user)
-def view():
-    story_id = request.json.get('id')
-    return dict(story=story)
+# To do: make a view_story button and page
+@action('view/<story_id:int>')
+@action.uses(db, session, auth.user, 'view.html')
+def view(story_id = None):
+    assert story_id is not None
+    rows = db(db.story.story_id == story_id).select()
+    story = rows[0]
+    return dict(story=story, url_signer=url_signer)
+
+# Dummy comment
+
+# @action('edit/<bird_id:int>', method=["GET", "POST"])
+# @action.uses('edit.html', url_signer, db, session, auth.user)
+# def edit(bird_id=None):
+#     assert bird_id is not None
+#     # We read the product being edited from the db.
+#     # b = db(db.bird.id == bird_id).select().first()
+#     b = db.bird[bird_id]
+#     if b is None:
+#         # Nothing found to be edited!
+#         redirect(URL('index'))
+#     # Edit form: it has record=
+#     form = Form(db.bird, record=b, deletable=False, csrf_session=session, formstyle=FormStyleBulma)
+#     if form.accepted:
+#         # The update already happened!
+#         redirect(URL('index'))
+#     return dict(form=form)
+
+# @action('delete/<bird_id:int>')
+# @action.uses(db, session, auth.user, url_signer.verify())
+# def delete(bird_id=None):
+#     assert bird_id is not None
+#     db(db.bird.id == bird_id).delete()
+#     redirect(URL('index'))
+
+# @action('inc/<bird_id:int>')
+# @action.uses(db, auth.user, url_signer.verify())
+# def inc(bird_id=None):
+#     assert bird_id is not None
+#     bird = db.bird[bird_id]
+#     db(db.bird.id == bird_id).update(n_sightings=bird.n_sightings + 1)
+#     redirect(URL('index'))
+
+# # This is an example only, to be used as inspiration for your code to increment the bird count.
+# # Note that the bird_id parameter ...
+# @action('capitalize/<bird_id:int>') # the :int means: please convert this to an int.
+# @action.uses(db, auth.user, url_signer.verify())
+# # ... has to match the bird_id parameter of the Python function here.
+# def capitalize(bird_id=None):
+#     assert bird_id is not None
+#     bird = db.bird[bird_id]
+#     db(db.bird.id == bird_id).update(bird_name=bird.bird_name.capitalize())
