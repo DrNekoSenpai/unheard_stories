6bd86dc19f3a687051e00cdaa1b0794054c736e7
DrNekoSenpai <haesungbecker@gmail.com>
2023-06-08 18:44:43 -0700

    search works

--- controllers.py
+++ controllers.py
@@ -22,11 +22,18 @@ def index():
 @action('get_feed', method="POST")
 @action.uses(db, auth.user, url_signer)
 def get_feed():
-    return dict(feed=db(db.story).select().as_list())
+    search = request.json.get('search')
+    # Initialize query to get all stories
+    query = db.story
+    if search: 
+        # Modify query to filter stories based on the search term in the title or content
+        query = (db.story.title.lower().contains(search.lower())) | (db.story.content.lower().contains(search.lower()))
+    # Fetch the first 20 stories that satisfy the query
+    return dict(feed=db(query).select(limitby=(0, 20)).as_list())
 
 @action('get_comments', method="POST")
 @action.uses(db, auth.user, url_signer)
-def get_feed():
+def get_comments():
     story_id = request.json.get('story_id')
     num_comments = request.json.get('num_comments')
     comments = db(db.comment.story_id == story_id).select().as_list()
@@ -35,16 +42,18 @@ def get_feed():
 @action('add_comment', method="POST")
 @action.uses(db, auth.user, url_signer)
 def add_comment():
+    story_id = request.json.get('story_id')
     db.comment.insert(
-        story_id        =request.json.get('story_id'),
-        content         =request.json.get('content'),
-        author          =auth.current_user.get('username'),
-        creation_date   =datetime.datetime.utcnow(),
-        likes           =0,
+        story_id        = story_id,
+        content         = request.json.get('content'),
+        author          = auth.current_user.get('username'),
+        creation_date   = datetime.datetime.utcnow(),
+        likes           = 0,
     )
 
-    # Increment the number of comments for the story
-    db(db.story.story_id == request.json.get('story_id')).update(num_comments=request.json.get('num_comments'))
+    # Count the number of comments for the story on the server-side
+    num_comments = db(db.comment.story_id == story_id).count()
+    db(db.story.story_id == story_id).update(num_comments=num_comments)
     return "ok"
 
 @action('add_story', method="POST")
